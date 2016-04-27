1d789261ac3a9d92a6fc0777d30a569efb2d73bb
Piotr Gregor <piotrek.gregor@gmail.com>
2016-04-27 23:58:00 +0100

    binary

--- atmega32a/blink_led_1/main.c
+++ atmega32a/blink_led_1/main.c
@@ -16,26 +16,26 @@
 int
 main(void)
 {
-    DDRA = 0xFF;            /* Nakes PORTA as output,
+    DDRA |= 0xFF;            /* Nakes PORTA as output,
                              * mark all 8 pins as output  */
 
     while (1)
     {
-        PORTA = 0x01;       /* turn 1st led on */
-        _delay_ms(500);     /* 0.5s delay */
-        PORTA = 0x02;       /* turn 2nd led on */
-        _delay_ms(500);     /* delay again */
-        PORTA = 0x04;       /* turn 3d led on */
-        _delay_ms(500);     /* delay again */
-        PORTA = 0x08;       /* turn 4th led on */
-        _delay_ms(500);     /* delay again */
-        PORTA = 0x10;       /* turn 5th led on */
-        _delay_ms(500);     /* delay again */
-        PORTA = 0x20;       /* turn 6th led on */
-        _delay_ms(500);     /* delay again */
-        PORTA = 0x40;       /* turn 7th led on */
-        _delay_ms(500);     /* delay again */
-        PORTA = 0x80;       /* turn 8th led on */
-        _delay_ms(500);     /* delay again */
+        PORTA = 0b00000001;       /* turn 1st led on */
+        _delay_ms(10);     /* 0.5s delay */
+        PORTA = 0b00000010;       /* turn 2nd led on */
+        _delay_ms(10);     /* delay again */
+        PORTA = 0b00000100;       /* turn 3d led on */
+        _delay_ms(10);     /* delay again */
+        PORTA = 0b00001000;       /* turn 4th led on */
+        _delay_ms(10);     /* delay again */
+        PORTA = 0b00010000;       /* turn 5th led on */
+        _delay_ms(10);     /* delay again */
+        PORTA = 0b00100000;       /* turn 6th led on */
+        _delay_ms(10);     /* delay again */
+        PORTA = 0b01000000;       /* turn 7th led on */
+        _delay_ms(10);     /* delay again */
+        PORTA = 0b10000000;       /* turn 8th led on */
+        _delay_ms(10);     /* delay again */
     }
 }
