62a586e3cd676a5631aa92b75d7fffe752be1797
Piotr Gregor <piotrek.gregor@gmail.com>
2016-04-27 18:49:14 +0100

    use all pins of port A

--- atmega32a/blink_led_1/main.c
+++ atmega32a/blink_led_1/main.c
@@ -16,13 +16,26 @@
 int
 main(void)
 {
-    DDRC = 0xFF;            /* Nakes PORTC as output */
+    DDRA = 0xFF;            /* Nakes PORTA as output,
+                             * mark all 8 pins as output  */
 
     while (1)
     {
-        PORTC = 0xFF;       /* turn all leds on */
+        PORTA = 0x01;       /* turn 1st led on */
         _delay_ms(500);     /* 0.5s delay */
-        PORTB = 0x00;       /* all leds off */
+        PORTA = 0x02;       /* turn 2nd led on */
+        _delay_ms(500);     /* delay again */
+        PORTA = 0x04;       /* turn 3d led on */
+        _delay_ms(500);     /* delay again */
+        PORTA = 0x08;       /* turn 4th led on */
+        _delay_ms(500);     /* delay again */
+        PORTA = 0x10;       /* turn 5th led on */
+        _delay_ms(500);     /* delay again */
+        PORTA = 0x20;       /* turn 6th led on */
+        _delay_ms(500);     /* delay again */
+        PORTA = 0x40;       /* turn 7th led on */
+        _delay_ms(500);     /* delay again */
+        PORTA = 0x80;       /* turn 8th led on */
         _delay_ms(500);     /* delay again */
     }
 }
