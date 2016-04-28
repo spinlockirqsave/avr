6ad8b61296f7b7afa916d9bd924c65416afcc6f1
Piotr Gregor <piotrek.gregor@gmail.com>
2016-04-28 01:09:01 +0100

    use __delay_ms with values < 262.14ms!

--- atmega32a/blink_led_1/main.c
+++ atmega32a/blink_led_1/main.c
@@ -9,6 +9,8 @@
 #include <util/delay.h>
 
 
+#define __DELAY_BACKWARD_COMPATIBLE__
+
 #ifndef F_CPU
 #define F_CPU 16000000UL    /* 16 MHz clock speed */
 #endif
@@ -16,26 +18,14 @@
 int
 main(void)
 {
-    DDRA |= 0xFF;            /* Nakes PORTA as output,
+    DDRA |= (1<<PA0);            /* Nakes PORTA as output,
                              * mark all 8 pins as output  */
 
     while (1)
     {
-        PORTA = 0b00000001;       /* turn 1st led on */
-        _delay_ms(10);     /* 0.5s delay */
-        PORTA = 0b00000010;       /* turn 2nd led on */
-        _delay_ms(10);     /* delay again */
-        PORTA = 0b00000100;       /* turn 3d led on */
-        _delay_ms(10);     /* delay again */
-        PORTA = 0b00001000;       /* turn 4th led on */
-        _delay_ms(10);     /* delay again */
-        PORTA = 0b00010000;       /* turn 5th led on */
-        _delay_ms(10);     /* delay again */
-        PORTA = 0b00100000;       /* turn 6th led on */
-        _delay_ms(10);     /* delay again */
-        PORTA = 0b01000000;       /* turn 7th led on */
-        _delay_ms(10);     /* delay again */
-        PORTA = 0b10000000;       /* turn 8th led on */
-        _delay_ms(10);     /* delay again */
+        PORTA |= (1<<PA0);         /* turn 1st led on */
+        _delay_ms(150);             /* 0.15s delay, is on by 10ms */
+        PORTA &= ~(1<<PA0);          /* turn led off */
+        _delay_ms(10);             /* delay 0.01s, is off by 150ms */
     }
 }
