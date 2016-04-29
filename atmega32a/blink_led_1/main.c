590351a1aa57b19f5179e771785428798fb15be4
Piotr Gregor <piotrek.gregor@gmail.com>
2016-04-29 20:08:43 +0100

    short delays

--- atmega32a/blink_led_1/main.c
+++ atmega32a/blink_led_1/main.c
@@ -22,20 +22,20 @@ main(void)
     while (1)
     {
         PORTA = 0x01;       /* turn 1st led on */
-        _delay_ms(50);     /* 0.5s delay */
+        _delay_ms(16);     /* 0.5s delay */
         PORTA = 0x02;       /* turn 2nd led on */
-        _delay_ms(50);     /* delay again */
+        _delay_ms(14);     /* delay again */
         PORTA = 0x04;       /* turn 3d led on */
-        _delay_ms(50);     /* delay again */
+        _delay_ms(12);     /* delay again */
         PORTA = 0x08;       /* turn 4th led on */
-        _delay_ms(50);     /* delay again */
+        _delay_ms(10);     /* delay again */
         PORTA = 0x10;       /* turn 5th led on */
-        _delay_ms(50);     /* delay again */
+        _delay_ms(8);     /* delay again */
         PORTA = 0x20;       /* turn 6th led on */
-        _delay_ms(50);     /* delay again */
+        _delay_ms(6);     /* delay again */
         PORTA = 0x40;       /* turn 7th led on */
-        _delay_ms(50);     /* delay again */
+        _delay_ms(4);     /* delay again */
         PORTA = 0x80;       /* turn 8th led on */
-        _delay_ms(50);     /* delay again */
+        _delay_ms(2);     /* delay again */
     }
 }
