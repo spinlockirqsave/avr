c1229736d4029f811f154ba1c72226bdc9bea813
Piotr Gregor <piotrek.gregor@gmail.com>
2016-06-23 22:37:28 +0100

    port 1

--- atmega32a/ddr_pin_port_1/main.c
+++ atmega32a/ddr_pin_port_1/main.c
@@ -16,19 +16,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
 int
-main(void)
-{
-    DDRC = 0x0F;    /* 00001111 */
-    PORTC = 0x0C;   /* set HIGH pin PC3 and PC2 00001100 */
+main(void) {
+    DDRC = 0x0F;    /* 00001111, set lower nibble of PORTC to OUTPUT, upper nibble to INPUT */
+    PORTC = 0x0C;   /* set HIGH PORT PC3 and PC2 00001100 */
 
     while(1) {
         if (PINC == 0b01000000) { /* lets assume a 4V supply comes to PORTC.6 and Vcc = 5V */
-            PORTC = 0x0B;       /* 00001011 but don't touch pin PC2 - it stays LOW */
+            PORTC = 0x0B;       /* 00001011 but don't touch PORT PC2 - it stays LOW */
             _delay_ms(1000);    /* delay 1s */
         } else {
-            PORTC = 0x00;
+            PORTC = 0x00;       /* turn off all PORTs */
         }
     }
 }
